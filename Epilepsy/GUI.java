b6dd2d704cb058682dacb6ef5719f855dabed7b5
TJTesla <71670757+TJTesla@users.noreply.github.com>
2020-11-06 13:10:06 +0100

    Add files via upload

---
+++ Epilepsy/GUI.java
@@ -0,0 +1,77 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+
+import java.util.Random;
+
+public class GUI extends JFrame implements ActionListener
+{
+    JLabel lbl = new JLabel();
+    JButton btn = new JButton("START");
+    Random rng = new Random();
+    Timer timerOne, timerTwo;
+    
+    Color[] colors = { Color.BLACK, Color.BLUE,
+                    Color.CYAN, Color.DARK_GRAY,
+                    Color.GRAY, Color.GREEN,
+                    Color.LIGHT_GRAY, Color.MAGENTA,
+                    Color.PINK, Color.RED,
+                    Color.ORANGE, Color.YELLOW };
+    
+    public GUI() {
+        super("EPILEPSIE!!!");
+        final int size = 700;
+        setSize(size,  size);
+        setVisible(true);
+        setResizable(false);
+        
+        lbl.setBounds(size/4, size/4, size/2, size/2);
+        lbl.setOpaque(true);
+        lbl.setVisible(true);
+        
+        btn.setBounds(0, 0, size, size);
+        btn.setActionCommand("Start");
+        btn.addActionListener(this);
+        btn.setVisible(true);
+        
+        timerOne = new Timer(10, new ActionListener() {
+            public void actionPerformed (ActionEvent evt) {
+                startEpilepsie();
+            }
+        } );
+        timerOne.stop();
+        
+        timerTwo = new Timer(37, new ActionListener() {
+            public void actionPerformed (ActionEvent evt) {
+                moveDecal();
+            }
+        } );
+        timerTwo.stop();
+        
+        add(lbl);
+        add(btn);
+    }
+    
+    public void actionPerformed(ActionEvent evt) {
+        if (evt.getActionCommand().equals("Start")) {
+            btn.setVisible(false);
+            lbl.setVisible(true);
+            timerOne.start();
+            try { Thread.sleep(rng.nextInt(30)); }
+            catch (Exception e) {};
+            timerTwo.start();
+        }
+    }
+    
+    private void moveDecal() {
+        
+    }
+    
+    private void startEpilepsie() {
+        int rndIndex = rng.nextInt(colors.length);
+        getContentPane().setBackground(colors[rndIndex]);
+        rndIndex = rng.nextInt(colors.length);
+        lbl.setBackground(colors[rndIndex]);
+    }
+}
